be442c6108c1ef34d86abbf27f07e9952d09f57d
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-04-06 12:30:38 +0200

    Added rotation methods to the Block.cs

--- Block.cs
+++ Block.cs
@@ -8,5 +8,53 @@ namespace Tetris
 {
     public abstract class Block
     {
+        protected abstract Position[][] Tiles { get; }
+        protected abstract Position StartOffset { get; }
+        public abstract int Id { get; }
+
+        private int rotationState;
+        private Position offset;
+
+        public Block()
+        {
+            offset = new Position(StartOffset.Row, StartOffset.Column);
+        }
+
+        /// Method returning grid positions of block
+        /// current rotation and offset are factored in
+        /// 
+        public IEnumerable<Position> TilePositions()
+        {
+            /// loop over current positions
+            foreach (Position p in Tiles[rotationState])
+            {
+                /// add row offset and column offset
+                yield return new Position(p.Row + offset.Row, p.Column + offset.Column);
+            }
+        }
+
+        /// Rotate the block 90 degrees clockwise
+        /// first we increament the current rotation state
+        /// going back to zero if in final state
+        /// 
+       public void RotateCW()
+        {
+            rotationState = (rotationState + 1) % Tiles.Length;
+        }
+        /// <summary>
+        /// Rotate counter clockwise
+        /// </summary>
+        public void RotateCCW()
+        {
+            if (rotationState == 0)
+            {
+                rotationState = Tiles.Length - 1;
+            }
+            else
+            {
+                rotationState--;
+            }
+        }
     }
 }
+ 
