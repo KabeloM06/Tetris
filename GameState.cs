92a1d15ab27fb5f8fc32c8c01328506d16378903
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-04-07 13:08:03 +0200

    Added methods to rotate and move blocks sideways

---
+++ GameState.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    public class GameState
+    {
+        private Block currentBlock;
+
+        public Block CurrentBlock
+        {
+            get => currentBlock;
+            private set
+            {
+                currentBlock = value;
+                currentBlock.Reset();
+            }
+        }
+
+        
+        public GameGrid GameGrid { get; }
+
+        public BlockQueue BlockQueue { get; }
+
+        public bool GameOver { get; private set; }
+
+
+        /// <summary>
+        /// initialise gamegride with 22 rows and 10 columns
+        /// start the block queue and current game block
+        /// </summary>
+        public GameState()
+        {
+            GameGrid = new GameGrid(22, 10);
+            BlockQueue = new BlockQueue();
+            CurrentBlock = BlockQueue.GetAndUpdate();
+        }
+
+        /// Check if current block is in a legal posotion or not
+        /// 
+        private bool BlockFits()
+        {
+            foreach (Position p in CurrentBlock.TilePositions())
+            {
+                /// If blocks are over other tiles
+                if (!GameGrid.IsEmpty(p.Row, p.Column))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        /// Rotate block clockWise
+        /// But only if it is possible to do in current location
+        /// otherwise rotate counter clock wise
+        public void RotateBlockCW()
+        {
+            CurrentBlock.RotateCW();
+
+            if (!BlockFits())
+            {
+                CurrentBlock.RotateCCW();
+            }
+        }
+
+        /// Rotate block counter clockWise
+        /// But only if it is possible to do in current location
+        /// otherwise rotate clock wise
+        public void RotateBlockCCW()
+        {
+            CurrentBlock.RotateCCW();
+
+            if (!BlockFits())
+            {
+                CurrentBlock.RotateCW();
+            }
+        }
+
+        /// Methods to move block left and right
+        /// if it moves into an illegal position, then we move it back
+        /// 
+        public void MoveBlockLeft()
+        {
+            CurrentBlock.Move(0, -1);
+
+            if (!BlockFits())
+            {
+                CurrentBlock.Move(0, 1);
+            }
+        }
+
+        public void MoveBlockRight()
+        {
+            CurrentBlock.Move(0, 1);
+
+            if (!BlockFits())
+            {
+                CurrentBlock.Move(0, -1);
+            }
+        }
+    }
+}
