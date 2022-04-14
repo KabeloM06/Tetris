fb2926faef08fa74cb1887d3585a961c6cec4068
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-04-14 11:09:26 +0200

    Set up image controls in canvas

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,86 @@ namespace Tetris
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Array containing tile images
+        /// Order should match tile id's
+        /// Zero is the empty tile
+        /// </summary>
+        private readonly ImageSource[] tileImages = new ImageSource[]
+        {
+            new BitmapImage(new Uri("Assets/TileEmpty.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TileCyan.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TileBlue.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TileOrange.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TileYellow.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TileGreen.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TilePurple.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TileRed.png", UriKind.Relative)),
+        };
+
+        /// <summary>
+        /// Array containing block images
+        /// Order should match block id's
+        /// Empty block is index Zero
+        /// 
+        /// </summary>
+        private readonly ImageSource[] blockImages = new ImageSource[]
+        {
+            new BitmapImage(new Uri("Assets/Block-Empty.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Block-I.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Block-J.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Block-L.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Block-O.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Block-S.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Block-T.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Block-Z.png", UriKind.Relative)),
+        };
+
+        /// <summary>
+        /// Two Dimensional array for image controls
+        /// </summary>
+        /// 
+        private readonly Image[,] imageControls;
+
+        /// GameState Oblect
+        /// 
+        private GameState gameState = new GameState();
         public MainWindow()
         {
             InitializeComponent();
+            imageControls = SetupGameCanvas(gameState.GameGrid);
+        }
+
+        /// Set up image controls in canvas
+        /// 
+        private Image[,] SetupGameCanvas(GameGrid grid)
+        {
+            Image[,] imageControls = new Image[grid.Rows, grid.Columns];
+            int cellSize = 25; //canvas width is 250 and height 500. So 10 cells wide and 20 high
+
+            /// Look through every row and col in the grid
+            /// for each position we then create a new image control at 25px hight and length
+            /// 
+
+            for (int r = 0; r < grid.Rows; r++)
+            {
+                for (int c = 0; c < grid.Columns; c++)
+                {
+                    Image imageControl = new Image
+                    {
+                        Width = cellSize,
+                        Height = cellSize,
+                    };
+
+                    Canvas.SetTop(imageControl, (r - 2) * cellSize);
+                    Canvas.SetLeft(imageControl, c * cellSize);
+
+                    GameCanvas.Children.Add(imageControl);
+                    imageControls[r, c] = imageControl;
+                }
+            }
+
+            return imageControls;
         }
     }
 }
