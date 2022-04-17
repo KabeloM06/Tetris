ce84b5cf6d92602902e9777a7ffa4f055fe41c5b
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-04-17 09:25:20 +0200

    fixed visuals to see which block killed us

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -91,7 +91,7 @@ namespace Tetris
                         Height = cellSize,
                     };
 
-                    Canvas.SetTop(imageControl, (r - 2) * cellSize);
+                    Canvas.SetTop(imageControl, (r - 2) * cellSize + 10);
                     Canvas.SetLeft(imageControl, c * cellSize);
 
                     GameCanvas.Children.Add(imageControl);
